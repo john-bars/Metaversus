1d97bccb39bd24bed17f4bc8aca781913b65380f
John Bars Quipia <john.barsq@gmail.com>
2023-08-09 08:05:13 +0800

    change NavBar to Navbar

--- sections/NavBar.jsx
+++ sections/NavBar.jsx
@@ -1,8 +1,17 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { navVariants } from "@/utils/motion";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center my-4">
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className="flex justify-between items-center my-4"
+    >
       <Image
         src="/search.svg"
         alt="search"
@@ -10,7 +19,7 @@ const Navbar = () => {
         height={24}
         className="object-contain"
       />
-      <h2 className="font-extrabold text-2xl leading-7">METAVERSUS</h2>
+      <h2 className="font-extrabold text-2xl leading-7 ">METAVERSUS</h2>
       <Image
         src="/menu.svg"
         alt="menu"
@@ -18,7 +27,7 @@ const Navbar = () => {
         height={24}
         className="object-contain"
       />
-    </div>
+    </motion.nav>
   );
 };
 
