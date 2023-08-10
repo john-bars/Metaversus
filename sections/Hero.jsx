f6ff6663f639ae2fa11a5be81c6596df9faf4d97
John Bars Quipia <john.barsq@gmail.com>
2023-08-10 19:32:11 +0800

    Move the previous css to the styles directory as "styles.heroDtext".

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -20,11 +20,7 @@ const Hero = () => {
           <motion.h1>METAVERSE</motion.h1>
           <motion.div className="flex flex-row justify-center items-center">
             <h1 className={styles.heroHeading}>Ma</h1>
-            <h1
-              className={`${styles.heroHeading} scale-x-[2] px-3.5 sm:px-4 md:px-7 lg:px-10 xl:px-11 2xl:px-12`}
-            >
-              D
-            </h1>
+            <h1 className={`${styles.heroHeading} ${styles.heroDText}`}>D</h1>
             <h1 className={styles.heroHeading}>Ness</h1>
           </motion.div>
         </div>
