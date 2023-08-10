d9d35fb1b8b2dd983fccb5868ee4735514acfe31
John Bars Quipia <john.barsq@gmail.com>
2023-08-10 08:00:36 +0800

    create a reusable heading styles

--- styles/index.js
+++ styles/index.js
@@ -1,3 +1,4 @@
-const styles = {
-  darkTextWhite: "dark:",
+export const styles = {
+  heroHeading:
+    "font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[160px] md:leading-[114px] sm:leading-[70px] leading-[64px] uppercase text-white",
 };
