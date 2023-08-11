369c02a11c74035800491848eecb200143438c66
John Bars Quipia <john.barsq@gmail.com>
2023-08-11 21:43:26 +0800

    put a gradient

--- sections/NavBar.jsx
+++ sections/NavBar.jsx
@@ -2,32 +2,35 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { navVariants } from "@/utils/motion";
+import { container, navVariants } from "@/utils/motion";
 
 const Navbar = () => {
   return (
-    <motion.nav
+    <motion.div
       variants={navVariants}
       initial="hidden"
       whileInView="show"
-      className="flex justify-between items-center my-4"
+      className="relative py-2"
     >
-      <Image
-        src="/search.svg"
-        alt="search"
-        width={24}
-        height={24}
-        className="object-contain"
-      />
-      <h2 className="font-extrabold text-2xl leading-7 ">METAVERSUS</h2>
-      <Image
-        src="/menu.svg"
-        alt="menu"
-        width={24}
-        height={24}
-        className="object-contain"
-      />
-    </motion.nav>
+      <div className="absolute w-1/2 inset-0 gradient-01 " />
+      <div className="flex justify-between items-center my-4">
+        <Image
+          src="/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+          className="object-contain"
+        />
+        <h2 className="font-extrabold text-2xl leading-7 ">METAVERSUS</h2>
+        <Image
+          src="/menu.svg"
+          alt="menu"
+          width={24}
+          height={24}
+          className="object-contain"
+        />
+      </div>
+    </motion.div>
   );
 };
 
