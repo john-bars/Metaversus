d9a9bbc7392ac87603d01e415910b647d53959b7
John Bars Quipia <john.barsq@gmail.com>
2023-08-11 15:10:41 +0800

    create a reusable animation functions'

--- utils/motion.js
+++ utils/motion.js
@@ -18,3 +18,32 @@ export const navVariants = {
     },
   },
 };
+
+export const container = (staggerChildren, delayChildren) => ({
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren,
+      delayChildren,
+    },
+  },
+});
+
+export const textVariant = (delay) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 1.25,
+      delay,
+    },
+  },
+});
