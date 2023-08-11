c3fb69fa33bc7d844811156dfd556e9905549b9f
John Bars Quipia <john.barsq@gmail.com>
2023-08-11 15:38:07 +0800

    rename NavBar to Navbar

--- sections/index.js
+++ sections/index.js
@@ -1,13 +1,13 @@
-import About from './About';
-import Explore from './Explore';
-import Feedback from './Feedback';
-import Footer from './Footer';
-import GetStarted from './GetStarted';
-import Hero from './Hero';
-import Insights from './Insights';
-import NavBar from './NavBar';
-import WhatsNew from './WhatsNew';
-import World from './World';
+import About from "./About";
+import Explore from "./Explore";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Insights from "./Insights";
+import Navbar from "./NavBar";
+import WhatsNew from "./WhatsNew";
+import World from "./World";
 
 export {
   About,
@@ -17,7 +17,7 @@ export {
   GetStarted,
   Hero,
   Insights,
-  NavBar,
+  Navbar,
   WhatsNew,
   World,
 };
