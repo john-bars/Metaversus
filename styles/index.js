4c05ab1bc2c64ff4e496eea6f6df62ef48c8cd47
John Bars Quipia <john.barsq@gmail.com>
2023-08-13 16:21:47 +0800

    Create a css for the letter 'D' in the MaDNess text in the Hero section.'

--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,5 @@
 export const styles = {
   heroHeading:
     "font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[160px] md:leading-[114px] sm:leading-[70px] leading-[64px] uppercase text-white",
-  heroDText: "scale-x-[2] px-3.5 sm:px-4 md:px-7 lg:px- xl:px-11 2xl:px-12",
+  heroDText: "scale-x-[2] px-3.5 sm:px-4 md:px-7 lg:px-10 xl:px-11 2xl:px-12",
 };
