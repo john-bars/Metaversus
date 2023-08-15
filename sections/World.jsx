be60b6236755b2f4cef5e35752f0e9e17f8fe19d
John Bars Quipia <john.barsq@gmail.com>
2023-08-15 21:13:05 +0800

    add a styling for the globe and people images.

--- sections/World.jsx
+++ sections/World.jsx
@@ -21,11 +21,23 @@ const World = () => {
           textStyles="text-center"
         />
 
-        <motion.div>
+        <motion.div className="relative mt-[68px] flex w-full h-[40vw] ">
           <Image src="/map.png" alt="map" fill />
-          <img src="/people-01.png" alt="people" />
-          <img src="/people-02.png" alt="people" />
-          <img src="/people-03.png" alt="people" />
+          <img
+            src="/people-01.png"
+            alt="people"
+            className="w-[5vw] h-[5vw] absolute bottom-[12%] right-[7%] p-[0.5vw] rounded-full bg-gray-500 "
+          />
+          <img
+            src="/people-02.png"
+            alt="people"
+            className="top-[10%] left-[10%] w-[5vw] h-[5vw] absolute  p-[0.5vw] rounded-full bg-gray-500 "
+          />
+          <img
+            src="/people-03.png"
+            alt="people"
+            className="top-[50%] left-[45%] w-[5vw] h-[5vw] absolute p-[0.5vw] rounded-full bg-gray-500 "
+          />
         </motion.div>
       </motion.div>
     </section>
