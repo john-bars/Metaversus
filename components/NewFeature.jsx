203752bab0f30b68798061af42aa9ea136df8025
John Bars Quipia <john.barsq@gmail.com>
2023-08-15 16:14:55 +0800

    provide the styling for the NewFeature component.

--- components/NewFeature.jsx
+++ components/NewFeature.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-
 const NewFeature = ({ imgUrl, title, subtitle }) => {
   return (
-    <div>
-      <div>
-        <img src={imgUrl} alt="icon" />
+    <div className="flex flex-col flex-1 sm:max-w-[250px] min-w-[210px]">
+      <div className="flex justify-center items-center w-[70px] h-[70px] rounded-3xl bg-sky-800 opacity-80">
+        <img src={imgUrl} alt="icon" className="oject-contain w-1/2 h-1/2" />
       </div>
-      <h1>Title {title}</h1>
-      <p>{subtitle}</p>
+      <h1 className="mt-7 font-bold text-2xl leading-7 text-slate-200">
+        Title {title}
+      </h1>
+      <p className="flex-1 mt-4 font-normal text-lg text-slate-300 leading-8">
+        {subtitle}
+      </p>
     </div>
   );
 };
