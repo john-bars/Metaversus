04716a9060656a32671078450231af9da58780f1
John Bars Quipia <john.barsq@gmail.com>
2023-08-15 16:04:15 +0800

    Create the basic structure of the WhatsNew section including the NewFeature, TypingText, and TitleText components.

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+"use client";
 
-function WhatsNew() {
-  return <div>WhatsNew</div>;
-}
+import { NewFeature, TitleText, TypingText } from "@/components";
+import { newFeatures } from "@/constants";
+import { motion } from "framer-motion";
+
+const WhatsNew = () => {
+  return (
+    <section>
+      <motion.div>
+        <motion.div>
+          <TypingText title="| What's New?" />
+          <TitleText title={<>What's new about Metaversus </>} />
+          <div>
+            {newFeatures.map((feature) => (
+              <NewFeature key={feature.title} {...feature} />
+            ))}
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default WhatsNew;
