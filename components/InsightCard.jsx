36a2c6f2cf6d57ec1de0cc2f5c144e1bbb5ed688
John Bars Quipia <john.barsq@gmail.com>
2023-08-16 16:50:51 +0800

    put the basic parts of the InsideCard component.

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const InsightCard = () => {
-  return <div>InsightCard</div>;
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motion";
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => {
+  return (
+    <motion.div>
+      <img src={imgUrl} alt="planet-01" />
+      <div>
+        <div>
+          <h4>{title}</h4>
+          <h4>{subtitle}</h4>
+        </div>
+        <div>
+          <img src="arrow.svg" alt="arrow" />
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default InsightCard;
