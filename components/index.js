9389dc4921ba9bc5534e28b0d3de16afc8542530
John Bars Quipia <john.barsq@gmail.com>
2023-08-16 13:27:23 +0800

    import and export the InsightCard component inside the components/index.js for organized indexing.

--- components/index.js
+++ components/index.js
@@ -2,5 +2,13 @@ import { TypingText, TitleText } from "./CustomText";
 import ExploreCard from "./ExploreCard";
 import StartSteps from "./StartSteps";
 import NewFeature from "./NewFeature";
+import InsightCard from "./InsightCard";
 
-export { TypingText, TitleText, ExploreCard, StartSteps, NewFeature };
+export {
+  TypingText,
+  TitleText,
+  ExploreCard,
+  StartSteps,
+  NewFeature,
+  InsightCard,
+};
