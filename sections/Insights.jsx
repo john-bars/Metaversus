3f5ea6bb06852ea37819f166bfbb05dc900a1fc7
John Bars Quipia <john.barsq@gmail.com>
2023-08-16 13:28:19 +0800

    add several components needed for the Insights section.

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+"use client";
 
-function Insights() {
-  return <div>Insights</div>;
-}
+import { motion } from "framer-motion";
+
+import { InsightCard, TitleText, TypingText } from "@/components";
+import { insights } from "@/constants";
+
+const Insights = () => {
+  return (
+    <section>
+      <motion.div>
+        <TypingText title={""} textStyles={""} />
+        <TitleText title={""} textStyles={""} />
+        <div>
+          {insights.map((insight, index) => (
+            <InsightCard />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Insights;
