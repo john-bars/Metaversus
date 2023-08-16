b70c0525e990cb59418db3ec388fe83fc43ed017
John Bars Quipia <john.barsq@gmail.com>
2023-08-16 17:18:47 +0800

    provide css styling.

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -7,11 +7,11 @@ import { insights } from "@/constants";
 
 const Insights = () => {
   return (
-    <section>
-      <motion.div>
-        <TypingText title={""} textStyles={""} />
-        <TitleText title={""} textStyles={""} />
-        <div>
+    <section className="relative z-0 sm:p-16 xs:p-8 px-6 py-12">
+      <motion.div className="flex flex-col mx-auto 2xl:max-w-[1280px] w-full">
+        <TypingText title="| Insight" textStyles="text-center" />
+        <TitleText title="Insight about metaverse" textStyles="text-center" />
+        <div className="flex flex-col mt-12 gap-[2rem]">
           {insights.map((insight, index) => (
             <InsightCard
               key={`insight-${index}`}
