94a2943296534aa6bf3864f002bf220d9e56beaf
John Bars Quipia <john.barsq@gmail.com>
2023-08-17 10:34:49 +0800

    fix the side scroll issue for small screens with the ommited "relative" position.

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ const Home = () => {
         <div className="gradient-03 z-0" />
         <Explore />
       </div>
-      <div>
+      <div className="relative">
         <GetStarted />
         <div className="gradient-04 z-0 " />
         <WhatsNew />
