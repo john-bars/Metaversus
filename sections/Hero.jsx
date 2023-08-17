b5b1d9bf3bf12756a98dae62a95aa18cf6424954
John Bars Quipia <john.barsq@gmail.com>
2023-08-17 20:30:22 +0800

    edit styling for different screen sized.

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { motion } from "framer-motion";
 
 import { container, textVariant, slideIn } from "@/utils/motion";
@@ -9,7 +8,7 @@ import { styles } from "@/styles";
 
 const Hero = () => {
   return (
-    <section className="sm:py-4 xs:py-2 px-1.5 py-3 mt-[10vh]">
+    <section className="sm:py-4 xs:py-2 py-3 mt-[10vh]">
       <motion.div
         variants={container}
         initial="hidden"
@@ -33,27 +32,27 @@ const Hero = () => {
 
         <motion.div
           variants={slideIn("right", "tween", 0.2, 1)}
-          className="relative w-full md:-mt-5 -mt-3 -top-8 "
+          className="relative w-full md:-mt-5 -mt-3 -top-4 md:-top-8 pl-[10vw]"
         >
-          {/* <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] -top-8 z-0 " /> */}
+          <div className="absolute w-full h-[260px] -top-5 sm:h-[370px] sm:-top-5 md:h-[530px] md:-top-8 hero-gradient rounded-tl-[140px]  z-0 " />
           <Image
             src="/cover.png"
             alt="cover"
             width={500}
             height={350}
-            className="w-full object-cover rounded-tl-[160px] rounded-bl-[50px] relative z-10 sm:h-[500px] xs:max-lg:ml-4 xl:ml-16 ml-16 "
+            className="w-full h-[250px] sm:h-[350px] md:h-[500px] object-cover rounded-tl-[140px] z-10 relative "
           />
 
           <a
             href="#explore"
-            className="flex justify-end  sm:-mt-[70px] -mt-[50px] mr-20"
+            className="flex justify-end  sm:-mt-[60px] md:-mt-[70px] -mt-[50px] pr-15vw"
           >
             <Image
               src="/stamp.png"
               alt="stamp"
               width={100}
               height={100}
-              className="sm:w-[155px] sm:h-[155px] object-contain z-10"
+              className="w-[90px] h-[90px] sm:w-[100px] sm:h-[100px] md:w-[155px] md:h-[155px] object-contain z-10"
             />
           </a>
         </motion.div>
